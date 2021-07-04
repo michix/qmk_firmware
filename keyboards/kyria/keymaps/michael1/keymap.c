ac0c20bda9b6e41a35bf7e2f64af8a7e00480a17
Michael Jaeger <michael.jaeger@tronsai.com>
2021-07-04 22:21:24 +0200

    chore: passt Formatierung an

--- keyboards/kyria/keymaps/michael1/keymap.c
+++ keyboards/kyria/keymaps/michael1/keymap.c
@@ -42,14 +42,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-      KC_ESC,                KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
-      KC_ESC,   HOME_A,   HOME_S, HOME_D, HOME_F, HOME_G,     HOME_H, HOME_J, HOME_K, HOME_L, HOME_SCLN, KC_QUOT,
-      LT(KC_LSFT, KC_GRV),              KC_Z,   KC_X, KC_C,   KC_V,   KC_B,   _______,   _______, _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+      KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,               KC_Y, KC_U, KC_I, KC_O, KC_P, KC_PIPE,
+      KC_ESC, HOME_A, HOME_S, HOME_D, HOME_F, HOME_G,     HOME_H, HOME_J, HOME_K, HOME_L, HOME_SCLN, KC_QUOT,
+      LT(KC_LSFT, KC_GRV), KC_Z, KC_X, KC_C, KC_V, KC_B, _______, _______, _______, _______, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
 
-      _______,  _______,
-        LT(_LAYER2, KC_SPC),  LT(_LAYER4, KC_ENT), LT(_LAYER3, KC_TAB),
-        LT(_LAYER3, KC_TAB), LT(_LAYER4, KC_DEL), LT(_LAYER1, KC_BSPACE),
-        _______, _______
+      _______,  _______, LT(_LAYER2, KC_SPC), LT(_LAYER4, KC_ENT), LT(_LAYER3, KC_TAB),
+      LT(_LAYER3, KC_TAB), LT(_LAYER4, KC_DEL), LT(_LAYER1, KC_BSPACE), _______, _______
     ),
 /*
  * Layer1: Symbols
