44e510446d16ba1d8bfc004ea588398a3c1dc598
Michael Jaeger <michael.jaeger@tronsai.com>
2021-07-04 22:14:41 +0200

    fix: schaltet encoder aus

--- keyboards/kyria/keymaps/michael1/keymap.c
+++ keyboards/kyria/keymaps/michael1/keymap.c
@@ -260,24 +260,24 @@ void oled_task_user(void) {
 }
 #endif
 
-#ifdef ENCODER_ENABLE
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        // Volume control
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-    else if (index == 1) {
-        // Page up/Page down
-        if (clockwise) {
-            tap_code(KC_DOWN);
-        } else {
-            tap_code(KC_UP);
-        }
-    }
-    return true;
-}
-#endif
+/*#ifdef ENCODER_ENABLE*/
+/*bool encoder_update_user(uint8_t index, bool clockwise) {*/
+    /*if (index == 0) {*/
+        /*// Volume control*/
+        /*if (clockwise) {*/
+            /*tap_code(KC_VOLU);*/
+        /*} else {*/
+            /*tap_code(KC_VOLD);*/
+        /*}*/
+    /*}*/
+    /*else if (index == 1) {*/
+        /*// Page up/Page down*/
+        /*if (clockwise) {*/
+            /*tap_code(KC_DOWN);*/
+        /*} else {*/
+            /*tap_code(KC_UP);*/
+        /*}*/
+    /*}*/
+    /*return true;*/
+/*}*/
+/*#endif*/
