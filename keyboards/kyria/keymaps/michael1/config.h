be32661c3ca5b466ea045f71fa256ffc7d8840c1
Michael Jaeger <michael.jaeger@tronsai.com>
2021-07-17 10:23:03 +0200

    feat: versucht, das Layout weiter zu optimieren

--- keyboards/kyria/keymaps/michael1/config.h
+++ keyboards/kyria/keymaps/michael1/config.h
@@ -34,16 +34,16 @@
 #define EE_HANDS
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 200
+#define TAPPING_TERM 170
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define TAPPING_FORCE_HOLD
+//#define TAPPING_FORCE_HOLD
 
 // Apply the modifier on keys that are tapped during a short hold of a modtap
-#define PERMISSIVE_HOLD
+//#define PERMISSIVE_HOLD
 
 #define COMBO_COUNT 2
 #define COMBO_TERM 300
--- keyboards/kyria/keymaps/michael1/keymap.c
+++ keyboards/kyria/keymaps/michael1/keymap.c
@@ -112,25 +112,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Base Layer: QWERTY
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |  ESC   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  | \   |
+ * |  ESC   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  NO    |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |  ESC   |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
  * |        |LShift|  ALT | GUI  | CTRL |ALTGR |                              |ALTGR | CTRL |  GUI |  ALT |RShift|  ' "   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |   `    |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | . >  | /  ? |  - _   |
+ * |   `    |   Z  |   X  |   C  |   V  |   B  | ESC  |      |  |      |  `   |   N  |   M  | ,  < | . >  | /  ? |  - _   |
  * | LSHIFT |      |      |      |ALTGR |      |      |      |  |      |      |      |ALTGR |      |      |      |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      | Teams|Space |Enter | TAB  |  | TAB  |Del   |Backsp|Teams |      |
+ *                        |      | Teams|Space |Enter | TAB  |  |  '   |Del   |Backsp|Teams |      |
  *                        |      | Share|Layr2 |Layr4 |Layer3|  |Layer3|Layr4 |Layr1 |Mute  |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-      KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,               KC_Y, KC_U, KC_I, KC_O, KC_P, KC_PIPE,
+      KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,               KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO,
       KC_ESC, HOME_A, HOME_S, HOME_D, HOME_F, HOME_G,     HOME_H, HOME_J, HOME_K, HOME_L, HOME_SCLN, KC_QUOT,
-      LT(KC_LSFT, KC_GRV), KC_Z, KC_X, KC_C, HOME_V, KC_B, _______, _______, _______, _______, KC_N, HOME_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
+      LT(KC_LSFT, KC_GRV), KC_Z, KC_X, KC_C, HOME_V, KC_B, KC_ESC, _______, _______, KC_GRV, KC_N, HOME_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
 
       RGB_MODE_FORWARD,  RCS(KC_E), LT(_LAYER2, KC_SPC), LT(_LAYER4, KC_ENT), LT(_LAYER3, KC_TAB),
-      LT(_LAYER3, KC_TAB), LT(_LAYER4, KC_DEL), LT(_LAYER1, KC_BSPACE), RCS(KC_M), RGB_MODE_REVERSE
+      LT(_LAYER3, KC_QUOT), LT(_LAYER4, KC_DEL), LT(_LAYER1, KC_BSPACE), RCS(KC_M), RGB_MODE_REVERSE
     ),
 /*
  * Layer1: Symbols
@@ -160,7 +160,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |        |  {   |  [   |  ]   |  }   |  =   |                              |  -   |  4   |  5   |  6   |  +   |  NO    |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |  (   |  <   |  >   |  )   |      |      |      |  |      |      |  0   |  1   |  2   |  3   |  /   |        |
+ * |        |  (   |  <   |  >   |  )   |  |   |      |      |  |      |      |  0   |  1   |  2   |  3   |  /   |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |  |      |Enter |  .   |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
@@ -169,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LAYER2] = LAYOUT(
       _______, KC_1, KC_2,  KC_3, KC_4,  KC_5,                                                  KC_6, KC_7, KC_8, KC_9,  KC_0, _______,
       _______, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_EQUAL,                                     KC_MINS, KC_4, KC_5,  KC_6, RSFT(KC_EQUAL),   KC_NO,
-      _______, KC_LPRN, LSFT(KC_COMM), LSFT(KC_DOT), KC_RPRN, _______, _______, _______, _______, _______, KC_0, KC_1,  KC_2, KC_3, KC_SLSH, _______,
+      _______, KC_LPRN, LSFT(KC_COMM), LSFT(KC_DOT), KC_RPRN, KC_PIPE, _______, _______, _______, _______, KC_0, KC_1,  KC_2, KC_3, KC_SLSH, _______,
                                  _______, _______, _______, _______, _______,  _______,  KC_ENT, KC_DOT, _______, _______
     ),
 /*
