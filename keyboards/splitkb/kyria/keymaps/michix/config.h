877903445bcfe023eca39128a94c1c4870f9256d
Michael Jaeger <michael.jaeger@tronsai.com>
2021-10-19 10:49:45 +0200

    feat: setzt TAPPING_TERM auf 150

--- keyboards/splitkb/kyria/keymaps/michix/config.h
+++ keyboards/splitkb/kyria/keymaps/michix/config.h
@@ -42,7 +42,7 @@
 #define EE_HANDS
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
